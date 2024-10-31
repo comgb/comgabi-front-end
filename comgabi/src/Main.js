53406ac141798dab7a2696bcb53d900055f87ab0
이예준 <pipining4470@naver.com>
2024-10-31 21:15:01 +0900

    ADD

---
+++ comgabi/src/Main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <>
+        <header>
+
+        </header>
+        <aside>
+
+        </aside>
+        <section>
+
+        </section>
+        <footer>
+            
+        </footer>
+        </>
+    )
+}
+
+export default Main
