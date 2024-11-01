9df8282d00c3d04311fc768902b9e3f108e34fe5
이예준 <pipining4470@naver.com>
2024-11-01 20:15:31 +0900

    UPDATE

--- comgabi/src/App.js
+++ comgabi/src/App.js
@@ -1,6 +1,9 @@
+import Main from "./Main";
+
 function App() {
   return (
     <>
+    <Main />
     </>
   );
 }
--- comgabi/src/Main.js
+++ comgabi/src/Main.js
@@ -3,18 +3,22 @@ import React from 'react'
 function Main() {
     return (
         <>
-        <header>
+            <header className='flex items-center h-12 bg-gray-300'>
+                <img src="img/comgabi-logo.png" alt="logo-img" className='h-7 ml-3'/>
+                <form action="#" className='ml-3%'>
+                    <input type="text" />
+                    <input type="submit" value="검색" />
+                </form>
+            </header>
+            <aside>
 
-        </header>
-        <aside>
+            </aside>
+            <section>
 
-        </aside>
-        <section>
+            </section>
+            <footer>
 
-        </section>
-        <footer>
-            
-        </footer>
+            </footer>
         </>
     )
 }
