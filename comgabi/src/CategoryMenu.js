cfc56c84f0469f330323ada33110323616c753a1
K-wooseok <kws022788@gmail.com>
2024-11-03 23:04:19 +0900

    STYLE

--- comgabi/src/CategoryMenu.js
+++ comgabi/src/CategoryMenu.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const CategoryMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -19,14 +20,20 @@ const CategoryMenu = () => {
                     </span>
                 </button>
 
-                <div className={`bg-gray-300 fixed inset-y-0 left-0 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 z-40 w-64`}>
+                <div className={`bg-blue-500 text-white fixed inset-y-0 left-0 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 z-40 w-64`}>
                     <nav className="flex flex-col items-center justify-center h-full">
-                        <ul className="space-y-5 text-lg font-bold">
-                            <li><a href="#" className="text-black">노트북</a></li>
-                            <li><a href="#" className="text-black">모니터</a></li>
-                            <li><a href="#" className="text-black">본체</a></li>
-                            <li><a href="#" className="text-black">기타</a></li>
-                        </ul>
+                        <Link to="/notebook" className="w-48 text-center font-bold text-xl px-16 py-4 border-b border-white last:border-b-0 transition-colors duration-300 hover:bg-white hover:text-blue-500">
+                            노트북
+                        </Link>
+                        <Link to="/monitor" className="w-48 text-center font-bold text-xl px-16 py-4 border-b border-white last:border-b-0 transition-colors duration-300 hover:bg-white hover:text-blue-500">
+                            모니터
+                        </Link>
+                        <Link to="/desktop" className="w-48 text-center font-bold text-xl px-16 py-4 border-b border-white last:border-b-0 transition-colors duration-300 hover:bg-white hover:text-blue-500">
+                            본체
+                        </Link>
+                        <Link to="/etc" className="w-48 text-center font-bold text-xl px-16 py-4 border-b border-white last:border-b-0 transition-colors duration-300 hover:bg-white hover:text-blue-500">
+                            기타
+                        </Link>
                     </nav>
                 </div>
             </div>
