8901bf81ca7dd2e13f7cc938471743db8505f0ed
K-wooseok <kws022788@gmail.com>
2024-11-03 15:51:43 +0900

    STYLE

--- comgabi/src/CategoryMenu.js
+++ comgabi/src/CategoryMenu.js
@@ -1,16 +1,35 @@
-import React from 'react';
-
-const categories = ['노트북', '모니터', '본체', '기타'];
+import React, { useState } from 'react';
 
 const CategoryMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <aside className="bg-blue-500 text-white w-64 p-5">
-            <h2 className="text-xl font-bold p-4 border-b border-white">카테고리</h2>
-            {categories.map((category, index) => (
-                <div key={index} className="p-4 border-b border-white last:border-b-0 transition-colors duration-300 hover:bg-white hover:text-blue-500"> {/* 이예준 aside를 햄버거 메뉴로 하는건 어떨까요?*/}
-                    {category}
+        <aside>
+            <div className="relative">
+                <button
+                    onClick={toggleMenu}
+                    className="fixed top-4 left-5 focus:outline-none z-50"
+                >
+                    <span className="material-symbols-outlined icon">
+                        {isOpen ? 'close' : 'menu'}
+                    </span>
+                </button>
+
+                <div className={`bg-gray-300 fixed inset-y-0 left-0 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 z-40 w-64`}>
+                    <nav className="flex flex-col items-center justify-center h-full">
+                        <ul className="space-y-5 text-lg font-bold">
+                            <li><a href="#" className="text-black">노트북</a></li>
+                            <li><a href="#" className="text-black">모니터</a></li>
+                            <li><a href="#" className="text-black">본체</a></li>
+                            <li><a href="#" className="text-black">기타</a></li>
+                        </ul>
+                    </nav>
                 </div>
-            ))}
+            </div>
         </aside>
     );
 };
