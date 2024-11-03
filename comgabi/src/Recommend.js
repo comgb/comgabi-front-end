4917d54a7fb8bb5efbbb88d4dd10b7489a625566
K-wooseok <kws022788@gmail.com>
2024-11-03 20:09:26 +0900

    ADD

---
+++ comgabi/src/Recommend.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Recommend = () => {
+    return (
+        <div className="justify-center border border-gray-300 rounded-lg mr-10 ml-10">
+            <h2 className="text-2xl font-bold p-2">추천 상품</h2>
+            <div className="grid grid-rows-2 grid-cols-2 text-center h-60">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Recommend;
