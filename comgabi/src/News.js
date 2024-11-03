d0798842bd0adac0fae88f1fa7d8fffa7e5efcab
K-wooseok <kws022788@gmail.com>
2024-11-03 20:09:18 +0900

    DELETE

--- comgabi/src/News.js
+++ comgabi/src/News.js
@@ -3,10 +3,10 @@ import React from 'react';
 const News = () => {
     return (
         <article className="flex h-96">
-            <div className="basis-[60%]  mx-auto my-10 p-6 border border-gray-300 rounded-lg shadow-lg">
+            <div className="basis-[60%]  mx-auto my-10 p-6 border border-gray-300 rounded-lg">
                 <h2 className="text-2xl font-bold mb-4">News</h2>   
             </div>
-            <div className="basis-[30%] mx-auto my-10 p-6 border border-gray-300 rounded-lg shadow-lg">
+            <div className="basis-[30%] mx-auto my-10 p-6 border border-gray-300 rounded-lg">
                 <h2 className="text-2xl font-bold mb-4">Youtube</h2>
             </div>
         </article>
