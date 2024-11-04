258b6463d43075e9770a7914a25ffb6043a95f9d
이예준 <pipining4470@naver.com>
2024-11-04 21:08:08 +0900

    CREATE
    footer추가

---
+++ comgabi/src/CopylightFooter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CopylightFooter() {
+    return (
+        <footer className='flex bg-gray-400 text-gray-700 py-8 justify-center items-center mt-10' style={{ height: '100px' }}>
+            <p>Copylight 2024. comgb Co. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default CopylightFooter
