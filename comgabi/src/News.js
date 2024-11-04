665043fb89841fd2689642443b268dc6081bbeaf
K-wooseok <kws022788@gmail.com>
2024-11-04 20:22:57 +0900

    STYLE

--- comgabi/src/News.js
+++ comgabi/src/News.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const News = () => {
     return (
-        <article className="flex h-96">
-            <div className="basis-[60%]  mx-auto my-10 p-6 border border-gray-300 rounded-lg">
+        <article className="grid gird-cols-1  sm:grid-cols-2 sm:h-auto h-96 gap-6 p-6 my-6">
+            <div className=" my-10 p-6 border border-gray-300 rounded-lg">
                 <h2 className="text-2xl font-bold mb-4">News</h2>   
             </div>
-            <div className="basis-[30%] mx-auto my-10 p-6 border border-gray-300 rounded-lg">
+            <div className=" my-10 p-6 border border-gray-300 rounded-lg">
                 <h2 className="text-2xl font-bold mb-4">Youtube</h2>
             </div>
         </article>
--- comgabi/src/Recommend.js
+++ comgabi/src/Recommend.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Recommend = () => {
     return (
-        <div className="justify-center mb-10 p-6 border border-gray-300 rounded-lg mr-10 ml-10">
+        <div className="justify-center p-6 border border-gray-300 rounded-lg mx-6">
             <h2 className="text-2xl font-bold">추천 상품</h2>
             <div className="grid grid-rows-2 grid-cols-2 text-center h-60">
                 <div></div>
