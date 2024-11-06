eaf9d6d5ba8745d98747006c69b074efd1ce4003
이예준 <pipining4470@naver.com>
2024-11-06 20:15:33 +0900

    UPDATE
    prettier 적용

--- comgabi/src/News.js
+++ comgabi/src/News.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React from "react";
 
 const News = () => {
-    return (
-        <article className="flex max-sm:flex-col h-auto gap-6 p-6 mt-6">
-            <div className="p-6 border border-gray-300 rounded-lg">
-                <h2 className="text-2xl font-bold mb-4">News</h2>
-                <table>
-                    <tr>
-                        <td className="mr-4">img</td>
-                        <td className="my-4 w-auto">headline</td>
-                        <td className="my-4 w-auto">press</td>
-                    </tr>
-                </table>
-            </div>
-            <div className="p-6 border border-gray-300 rounded-lg">
-                <h2 className="text-2xl font-bold mb-4">Youtube</h2>
-                <div className="grid grid-rows-2 gap-6">
-                    <iframe width="100%" height="100%" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    <iframe width="100%" height="100%" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                </div>
-            </div>
-        </article>
-    );
+  return (
+    <article className="flex max-sm:flex-col h-auto gap-6 p-6 mt-6">
+      <div className="p-6 border border-gray-300 rounded-lg">
+        <h2 className="text-2xl font-bold mb-4">News</h2>
+        <table>
+          <tr>
+            <td className="mr-4">img</td>
+            <td className="my-4 w-auto">headline</td>
+            <td className="my-4 w-auto">press</td>
+          </tr>
+        </table>
+      </div>
+      <div className="p-6 border border-gray-300 rounded-lg">
+        <h2 className="text-2xl font-bold mb-4">Youtube</h2>
+        <div className="grid grid-rows-2 gap-6">
+          <iframe
+            width="100%"
+            height="100%"
+            src=""
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            width="100%"
+            height="100%"
+            src=""
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default News;
--- comgabi/src/Recommend.js
+++ comgabi/src/Recommend.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Recommend = () => {
-    return (
-        <div className="justify-center p-6 border border-gray-300 rounded-lg mx-6">
-            <h2 className="text-2xl font-bold">추천 상품</h2>
-            <div className="grid grid-rows-2 grid-cols-2 text-center h-60">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="justify-center p-6 border border-gray-300 rounded-lg mx-6">
+      <h2 className="text-2xl font-bold">추천 상품</h2>
+      <div className="grid grid-rows-2 grid-cols-2 text-center h-60">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Recommend;
