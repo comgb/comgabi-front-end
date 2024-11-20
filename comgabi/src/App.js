0e1919fab08853864132d0e25e3705342cd41583
이예준 <pipining4470@naver.com>
2024-11-21 01:11:56 +0900

    UPDATE
    api연동

--- comgabi/src/App.js
+++ comgabi/src/App.js
@@ -1,18 +1,22 @@
 import { Link, Route, Routes } from "react-router-dom";
-import CategoryMenu from "./CategoryMenu";
-import HeaderMenuBar from "./HeaderMenuBar";
-import MainPage from "./MainPage";
-import ProductListPage from "./ProductListPage";
-import CopylightFooter from "./CopylightFooter";
-import ProductDetailsPage from "./ProductDetailsPage";
-import TopButton from "./TopButton";
-import LoginPage from "./LoginPage";
-import SignUpPage from "./SignUpPage";
-import SignUpPhone from "./SignUpPhone";
-import SignUpEmail from "./SignUpEmail";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import CategoryMenu from "./layout/CategoryMenu";
+import HeaderMenuBar from "./layout/HeaderMenuBar";
+import MainPage from "./mainpage/MainPage";
+import ProductListPage from "./list-page/ProductListPage";
+import CopylightFooter from "./layout/CopylightFooter";
+import ProductDetailsPage from "./product-page/ProductDetailsPage";
+import TopButton from "./layout/TopButton";
+import LoginPage from "./login/LoginPage";
+import SignUpPage from "./login/SignUpPage";
+import SignUpPhone from "./login/SignUpPhone";
+import SignUpEmail from "./login/SignUpEmail";
 function App() {
+
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <HeaderMenuBar />
       <CategoryMenu />
       <Routes>
@@ -26,7 +30,7 @@ function App() {
       </Routes>
       <TopButton />
       <CopylightFooter />
-    </>
+    </QueryClientProvider>
   );
 }
 
